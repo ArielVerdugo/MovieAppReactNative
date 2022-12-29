798f87a14bc6d75f167615792708be29ac3319ff
Ariel Verdugo <arielverdugo@MacBook-Pro-de-Ariel-2.local>
2022-12-29 11:25:34 -0300

    delete unused imports

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,10 +1,10 @@
 /* eslint-disable react-native-a11y/has-valid-accessibility-ignores-invert-colors */
-import React, { Component, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View, FlatList, Image, ActivityIndicator, TouchableHighlight } from 'react-native';
 import { useQuery } from '@tanstack/react-query';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { useDispatch, useSelector } from 'react-redux';
-//import NetInfo from '@react-native-community/netinfo';
+import NetInfo from '@react-native-community/netinfo';
 import { styles } from '@/screens/Home/Home.styles';
 import { IMAGE_URL, routes } from '@/controllers/routes';
 import { NAVIGATION } from '@/constants';
@@ -16,43 +16,38 @@ import { saveMovie } from '@/actions/MovieActions';
 
 export function Home({ navigation }) {
   const movieController = new MovieController(networkService);
-  const { isLoading, data } = useQuery(['allMovies'], movieController.getMovies);
+  const [isConnected, setIsConected] = useState(false);
+  const [date, setDate] = useState(new Date());
+  //const { isLoading, data } = useQuery(['allMovies'], movieController.getMovies, {
+  //cacheTime: 0,
+  //});
+
+  const { isLoading, data, isError } = useQuery(['allMovies'], movieController.getMovies);
 
   const dispatch = useDispatch();
   const moviesFav = useSelector(getFav);
 
-  /*NetInfo.fetch().then((state) => {
-    console.log('el estado es ' + state);
-    if (state.isConnected) {
-      currentSuccesDate = new Date().toLocaleDateString;
-      console.log('conectado ' + state);
-    } else {
-      var currentNoInternetDate = new Date().toLocaleDateString;
-      date = dateDiffInDays(currentSuccesDate, currentNoInternetDate);
-      console.log('desconectado ' + state);
+  useEffect(() => {
+    if (!isError) {
+      setDate(new Date());
     }
-  });
-
-  if (date > 1) {
-    return <Text style={styles.name}>No hay data</Text>;
-  }*/
-
-  /*const getNetInfo = () => {
-    // To get the network state once
-    NetInfo.fetch().then((state) => {
-      alert(
-        `Connection type: ${state.type}
-        Is connected?: ${state.isConnected}
-        IP Address: ${state.details.ipAddress}`,
-      );
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      setIsConected(state.isConnected);
     });
-  };*/
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   const dateDiffInDays = (a, b) => {
     const _MS_PER_DAY = 1000 * 60 * 60 * 24;
     const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
     const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
-    return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+    if (Math.floor((utc2 - utc1) / _MS_PER_DAY) < 2) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
   if (isLoading) {
@@ -92,16 +87,28 @@ export function Home({ navigation }) {
     }
   };
 
-  return (
-    <View>
-      <FlatList
-        ListHeaderComponentStyle={styles.listHeader}
-        ListHeaderComponent={headerComponent}
-        data={data.data.results}
-        renderItem={onMovie}
-        ItemSeparatorComponent={itemSeparator}
-        ListEmptyComponent={emptyComponent}
-      />
-    </View>
-  );
+  // eslint-disable-next-line eqeqeq
+  if (
+    isConnected == true ||
+    (isConnected != true && data != null && dateDiffInDays(date, new Date()))
+  ) {
+    return (
+      <View>
+        <FlatList
+          ListHeaderComponentStyle={styles.listHeader}
+          ListHeaderComponent={headerComponent}
+          data={data.data.results}
+          renderItem={onMovie}
+          ItemSeparatorComponent={itemSeparator}
+          ListEmptyComponent={emptyComponent}
+        />
+      </View>
+    );
+  } else {
+    return (
+      <View>
+        <Text> NO HAY NADA QUE MOSTRAR </Text>
+      </View>
+    );
+  }
 }
