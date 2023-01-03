1358d8140b5533320c26f934e08c50a102937194
Ariel Verdugo <arielverdugo@MacBook-Pro-de-Ariel-2.local>
2023-01-03 18:27:46 -0300

    desing home screen

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { Text, View, FlatList, Image, ActivityIndicator, TouchableHighlight } from 'react-native';
+import {
+  Text,
+  View,
+  FlatList,
+  Image,
+  ActivityIndicator,
+  TouchableHighlight,
+  ImageBackground,
+} from 'react-native';
 import { useQuery } from '@tanstack/react-query';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { useDispatch, useSelector } from 'react-redux';
@@ -14,6 +22,15 @@ import { addIcon } from '@/assets';
 import { saveMovie } from '@/actions/MovieActions';
 import { isOneDayDiff } from '@/utils/utils';
 import { EMPTY_MOVIES, MOVIES } from '@/constants/en';
+
+const imgae_background =
+  '/Users/arielverdugo/ejerciciosReactNative/MovieAppReactNative/src/assets/img/background_home.png/';
+const icon_plus =
+  '/Users/arielverdugo/ejerciciosReactNative/MovieAppReactNative/src/assets/img/Path.png';
+const icon_play =
+  '/Users/arielverdugo/ejerciciosReactNative/MovieAppReactNative/src/assets/img/Shape.png';
+const icon_info =
+  '/Users/arielverdugo/ejerciciosReactNative/MovieAppReactNative/src/assets/img/Shape-3.png';
 
 export function Home({ navigation }) {
   const movieController = new MovieController(networkService);
@@ -88,14 +105,52 @@ export function Home({ navigation }) {
     (isConnected !== true && data != null && isOneDayDiff(date, new Date()))
   ) {
     return (
-      <FlatList
+      /*<FlatList
         ListHeaderComponentStyle={styles.listHeader}
         ListHeaderComponent={headerComponent}
         data={data.data.results}
         renderItem={Movie}
         ItemSeparatorComponent={itemSeparator}
         ListEmptyComponent={emptyComponent}
-      />
+      />*/
+      <View style={styles.container}>
+        <ImageBackground source={require(imgae_background)} resizeMode="cover" style={styles.image}>
+          <View>
+            <View style={styles.container_text}>
+              <Text style={styles.text}>Kids</Text>
+              <View style={styles.circle} />
+              <Text style={styles.text}>Fantasy Movie</Text>
+              <View style={styles.circle} />
+              <Text style={styles.text}>Action</Text>
+            </View>
+            <View style={styles.container_banner}>
+              <Text style={styles.banner}>MOVY ORIGINAL</Text>
+            </View>
+            <View style={styles.container_icons} accessibilityIgnoresInvertColors={true}>
+              <Image
+                style={styles.plus_icon}
+                source={require(icon_plus)}
+                accessibilityIgnoresInvertColors={true}
+              />
+              <Image
+                style={styles.play_icon}
+                source={require(icon_play)}
+                accessibilityIgnoresInvertColors={true}
+              />
+              <Image
+                style={styles.info_icon}
+                source={require(icon_info)}
+                accessibilityIgnoresInvertColors={true}
+              />
+            </View>
+            <View style={styles.container_icon_text} accessibilityIgnoresInvertColors={true}>
+              <Text style={styles.icon_text_plus}>My list</Text>
+              <Text style={styles.icon_text_play}>Play</Text>
+              <Text style={styles.icon_text_info}>Info</Text>
+            </View>
+          </View>
+        </ImageBackground>
+      </View>
     );
   } else {
     return <Text>{EMPTY_MOVIES}</Text>;
--- src/screens/Home/Home.styles.js
+++ src/screens/Home/Home.styles.js
@@ -44,4 +44,101 @@ export const styles = StyleSheet.create({
     fontSize: 16,
     marginLeft: 13,
   },
+
+  container: {
+    flex: 1,
+    backgroundColor: '#000000',
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    height: '60%',
+    justifyContent: 'center',
+  },
+  container_text: {
+    height: 100,
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 30,
+  },
+  text: {
+    color: 'white',
+    fontSize: 12,
+    paddingLeft: 40,
+  },
+
+  circle: {
+    width: 8,
+    height: 8,
+    borderRadius: 8 / 2,
+    backgroundColor: 'white',
+    borderColor: 'black',
+    fontWeight: 'bold',
+    marginLeft: 35,
+    borderWidth: 1,
+  },
+
+  container_banner: {
+    height: 100,
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 1,
+  },
+
+  banner: {
+    fontSize: 13,
+    marginLeft: 90,
+    paddingLeft: 60,
+    paddingRight: 60,
+    marginTop: 10,
+    position: 'absolute',
+    color: 'rgba(5, 130, 255, 1)',
+    backgroundColor: 'rgba(5, 120, 255, 0.4)',
+    paddingBottom: 5,
+  },
+
+  container_icons: {
+    height: 100,
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 1,
+    paddingTop: 60,
+    marginLeft: 75,
+  },
+
+  plus_icon: {
+    alignContent: 'flex-start',
+    marginLeft: 10,
+  },
+
+  play_icon: {
+    alignContent: 'center',
+    marginLeft: 60,
+  },
+
+  info_icon: {
+    marginLeft: 65,
+  },
+
+  container_icon_text: {
+    height: 100,
+    flexDirection: 'row',
+    position: 'absolute',
+  },
+
+  icon_text_plus: {
+    color: 'white',
+    fontSize: 12,
+    marginLeft: 85,
+  },
+  icon_text_play: {
+    color: 'white',
+    fontSize: 12,
+    marginLeft: 60,
+  },
+  icon_text_info: {
+    color: 'white',
+    fontSize: 12,
+    marginLeft: 78,
+  },
 });
