87196a603e6bf183771e1c44ad0823a9c73e590d
ArielVerdugo <45951048+ArielVerdugo@users.noreply.github.com>
2023-01-24 14:53:06 -0300

    Add navigation back button and margins in favorites section (#7)
    Co-authored-by: Ariel Verdugo <arielverdugo@MacBook-Pro-de-Ariel-2.local>

--- src/navigation/FavoritesNavigator.js
+++ src/navigation/FavoritesNavigator.js
@@ -1,12 +1,15 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Image, Text, View } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { backIcon } from '@/assets';
 import { NAVIGATION } from '@/constants';
 import { MY_LIST } from '@/constants/en';
 import { Favorites } from '@/screens/Favorites/Favorites';
 import { styles } from '@/screens/Favorites/Favorites.styles';
 import { blackColor } from '@/constants/colors';
+import { TABS } from '@/constants/navigation';
 
 const Stack = createNativeStackNavigator();
 
@@ -28,14 +31,22 @@ export function FavoritesNavigator() {
 }
 
 function FavoritesHeader() {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
-      <View style={styles.containerIcon}>
-        <Image
-          style={styles.iconHeader}
-          source={backIcon}
+      <View>
+        <TouchableWithoutFeedback
+          accessibilityRole="button"
+          onPress={() => navigation.jumpTo(TABS.home)}
           accessibilityIgnoresInvertColors={true}
-        />
+        >
+          <Image
+            style={styles.iconHeader}
+            source={backIcon}
+            accessibilityIgnoresInvertColors={true}
+          />
+        </TouchableWithoutFeedback>
       </View>
       <View style={styles.containerText}>
         <Text style={styles.textBack}>{MY_LIST}</Text>
